0409cb46b94c8fd32749c6e4905be26e57b8f5ba
Dexon <h3d3r24@gmail.com>
2023-10-25 18:28:59 +0200

    items

---
+++ reference másolata.js
@@ -0,0 +1,170 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+let langs = document.querySelector(".language");
+
+let defaultLanguage = navigator.language.toUpperCase();
+if(defaultLanguage != "HU" && defaultLanguage != "EN")
+    defaultLanguage = "EN";
+
+let typed = new Typed(".multiple-text", {
+    strings: ["", "", "", ""],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+langs.addEventListener("click", () =>{
+    console.log(langs.getAttribute("class"));
+    if(langs.textContent == "HU"){
+        langs.textContent = "EN";
+    }
+    else {
+        langs.textContent = "HU";
+    }
+
+    setLanguage(langs.textContent);
+})
+
+const langData = {
+    HU:{
+        mlHome: "Kezdőlap",
+        mlAbout: "Rólam",
+        mlAboutDesc: "19 éves vagyok, több éves tapasztalatom van AMXX Pawn és SQL nyelvekből, emelett Python, C++, HTML & CSS valamint Javascript sem áll távol a tudásomtól.",
+        mlServices: "Szolgáltatások",
+        mlPortfolio: "Referencia",
+        mlContact: "Kapcsolat",
+        mlWelcome: "Üdvözöllek, A Nevem",
+        mlName: "Szabó Gergő",
+        mlShortIntro: "A programozáshoz való szenvedélyem miatt sok programnyelvet ismertem meg és sajátítottam el, valamint fogok is. Többek között a HTML, CSS, SQL és Javascript nyelvek, valamint az AMXX Pawn scriptnyelv áll hozzám a legközelebb.",
+        mlDesc1: "És Én Egy",
+        mlProf1: "Web fejlesztő",
+        mlProf2: "AMXX Pawn fejlesztő",
+        mlProf3: "Videóvágó",
+        mlTitleProf1: "Web Fejlesztés",
+        mlTitleProf2: "CS 1.6 Fejlesztés",
+        mlDescProf1: "Első referencia munkám az általad látott weboldal. Javascript, HTML és CSS használatával sok kreatív munkát lehet létrehozni, és akár Te is igényelhetsz ilyet.",
+        mlDescProf2: "Több szervert is készítettem, mint például Sniper, Onlyd2 valamint Zombit. Mivel bonyolultabb területekre is kanyarodtam, akárcsak az A* útvonalkeresés vagy az NPC rendszer, ezért sok egyedi ötlettel, kreatív megoldásokkal tudok segíteni hogy elkészüljön a vizualizációd.",
+        mlReference: "Referencia",
+        mlServices2: "Szolgáltatások",
+        mlAbout2: "Rólam",
+        mlContact2: "Kapcsolat",
+        mlFullName: "Teljes név",
+        mlMobileNumber: "Telefonszám",
+        mlSubject: "Tárgy",
+        mlMessage: "Üzenet"
+    },
+
+    EN:{
+        mlHome: "Home",
+        mlAbout: "About",
+        mlAboutDesc: "I'm 19 years old, I have multiple years of experience in regards to AMXX Pawn and SQL langauges, also Python, C++, HTML & CSS are not forbidden in my knowledge.",
+        mlServices: "Services",
+        mlPortfolio: "Reference",
+        mlContact: "Contact",
+        mlWelcome: "Hello, My Name Is",
+        mlName: "Gergő Szabó",
+        mlShortIntro: "Due to my obsession with programming, I have mastered many languages and will continue to do so. Among others, mostly the HTML, CSS, SQL and Javascript langauges, and the AMXX Pawn script language stands the closest to myself.",
+        mlDesc1: "And I'm a",
+        mlProf1: "Web Developer",
+        mlProf2: "AMXX Pawn Developer",
+        mlProf3: "Videóvágó",
+        mlTitleProf1: "Web Development",
+        mlTitleProf2: "CS 1.6 Development",
+        mlDescProf1: "Első referencia munkám az általad látott weboldal. Javascript, HTML és CSS használatával sok kreatív munkát lehet létrehozni, és akár Te is igényelhetsz ilyet.",
+        mlDescProf2: "I have already made a bunch of servers, e.g. Sniper, Onlyd2 and also Zombie. Since I tried my skills in many areas like A* pathfinding, NPC system, with many unique ideas and creative solutions I can help you to make the best of your vision.",
+        mlReference: "Reference",
+        mlServices2: "Services",
+        mlAbout2: "About",
+        mlContact2: "Contact",
+        mlFullName: "Full Name",
+        mlMobileNumber: "Mobile Number",
+        mlSubject: "Subject",
+        mlMessage: "Message"
+    }
+}
+
+setLanguage(defaultLanguage);
+
+function setLanguage(language){
+    console.log(language);
+    const languageData = langData[language];
+
+    typed.strings =[languageData["mlProf1"], languageData["mlProf2"], languageData["mlProf3"], languageData["mlProf1"], languageData["mlProf2"], languageData["mlProf3"]];
+
+    for (const key in languageData) {
+        if (languageData.hasOwnProperty(key)) {
+            const value = languageData[key];
+            //console.log(`Language: ${language}, Key: ${key}, Value: ${value}`);
+
+            if(document.getElementById(key)){
+                document.getElementById(key).textContent = value;
+            }
+            // You can perform further operations with the language, key, and value here
+        }
+    }
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+}
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () =>{
+    sections.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 150;
+        let height = section.offsetHeight;
+        let id = section.getAttribute("id");
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove("active");
+                document.querySelector("header nav a[href*=" + id + "]").classList.add("active")
+            })
+        }
+    })
+
+    
+    let header = document.querySelector("header");
+
+    header.classList.toggle("sticky", window.scrollY > 100);
+
+    menuIcon.classList.remove("bx-x");
+    navbar.classList.remove("active");
+}
+
+ScrollReveal({
+    //reset: true,
+    distance: "80px",
+    duration: 2000,
+    delay: 200
+})
+
+ScrollReveal().reveal(".home-content, .heading", {origin: "top"});
+ScrollReveal().reveal(".home-img, .services-container, .portfolio-box, .contact form", {origin: "bottom"});
+ScrollReveal().reveal(".home-content h1, .about-img", {origin: "left"});
+ScrollReveal().reveal(".home-content p, .about-content", {origin: "right"});
+
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    
+    const formData = new FormData(event.target);
+    const userData = {};
+
+    for (const [key, value] of formData) {
+        userData[key] = value;
+    }
+
+    localStorage.setItem('userData', JSON.stringify(userData));
+    console.log('User data saved:', userData);
+    
+    event.target.reset();
+});
+
+const savedData = localStorage.getItem('userData');
+console.log(savedData);
---
+++ reference.js
@@ -0,0 +1,174 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+let langs = document.querySelector(".language");
+
+let defaultLanguage = navigator.language.toUpperCase();
+if(defaultLanguage != "HU" && defaultLanguage != "EN")
+    defaultLanguage = "EN";
+
+let typed = new Typed(".multiple-text", {
+    strings: ["", "", "", ""],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+langs.addEventListener("click", () =>{
+    console.log(langs.getAttribute("class"));
+    if(langs.textContent == "HU"){
+        langs.textContent = "EN";
+    }
+    else {
+        langs.textContent = "HU";
+    }
+
+    setLanguage(langs.textContent);
+})
+
+const langData = {
+    HU:{
+        mlHome: "Kezdőlap",
+        mlAbout: "Rólam",
+        mlAboutDesc: "19 éves vagyok, több évnyi tapasztalattal.",
+        mlServices: "Szolgáltatások",
+        mlPortfolio: "Referencia",
+        mlContact: "Kapcsolat",
+        mlWelcome: "Üdvözöllek, A Nevem",
+        mlName: "Szabó Gergő",
+        mlShortIntro: "A programozáshoz való szenvedélyem miatt sok programnyelvet ismertem meg és sajátítottam el, valamint fogok is. Többek között a HTML, CSS, SQL és Javascript nyelvek, valamint az AMXX Pawn scriptnyelv áll hozzám a legközelebb.",
+        mlDesc1: "És Én Egy",
+        mlProf1: "Web fejlesztő",
+        mlProf2: "AMXX Pawn fejlesztő",
+        mlProf3: "Videóvágó",
+        mlTitleProf1: "Web Fejlesztés",
+        mlTitleProf2: "CS 1.6 Fejlesztés",
+        mlTitleProf3: "Videóvágás",
+        mlDescProf1: "Első referencia munkám az általad látott weboldal. Javascript, HTML és CSS használatával sok kreatív munkát lehet létrehozni, és akár Te is igényelhetsz ilyet.",
+        mlDescProf2: "Főbb referenciáim OnlyD2, Sniper és Zombi szerverek. Bonyolult, komplex, optimalizált pluginok, A* útvonalkeresés, NPC rendszer.",
+        mlDescProf3: "Sok tapasztalatom / referenciám van videóvágás terén. A lent látott kontentek hosszú, precíz munka eredményei.",
+        mlReference: "Referencia",
+        mlServices2: "Szolgáltatások",
+        mlAbout2: "Rólam",
+        mlContact2: "Kapcsolat",
+        mlFullName: "Teljes név",
+        mlMobileNumber: "Telefonszám",
+        mlSubject: "Tárgy",
+        mlMessage: "Üzenet"
+    },
+
+    EN:{
+        mlHome: "Home",
+        mlAbout: "About",
+        mlAboutDesc: "19 years old, serveral years experience.",
+        mlServices: "Services",
+        mlPortfolio: "Reference",
+        mlContact: "Contact",
+        mlWelcome: "Hello, My Name Is",
+        mlName: "Gergő Szabó",
+        mlShortIntro: "Due to my obsession with programming, I have mastered many languages and will continue to do so. Among others, mostly the HTML, CSS, SQL and Javascript langauges, and the AMXX Pawn script language stands the closest to myself.",
+        mlDesc1: "And I'm a",
+        mlProf1: "Web Developer",
+        mlProf2: "AMXX Pawn Developer",
+        mlProf3: "Video Editor",
+        mlTitleProf1: "Web Development",
+        mlTitleProf2: "CS 1.6 Development",
+        mlTitleProf3: "Video Editing",
+        mlDescProf1: "The first reference here is the portfolio itself. I created it with the languages HTML, CSS and Javascript. If you wish to, I can create your own.",
+        mlDescProf2: "My main references are OnlyD2, Sniper és Zombi servers. Hard, complex, optimalised plugins. A* pathfinding, NPC system.",
+        mlDescProf3: "I have a lot of reference to show you below. All the seen content the result of precise, hard work.",
+        mlReference: "Reference",
+        mlServices2: "Services",
+        mlAbout2: "About",
+        mlContact2: "Contact",
+        mlFullName: "Full Name",
+        mlMobileNumber: "Mobile Number",
+        mlSubject: "Subject",
+        mlMessage: "Message"
+    }
+}
+
+setLanguage(defaultLanguage);
+
+function setLanguage(language){
+    console.log(language);
+    const languageData = langData[language];
+
+    typed.strings =[languageData["mlProf2"], languageData["mlProf3"], languageData["mlProf1"], languageData["mlProf3"]];
+
+    for (const key in languageData) {
+        if (languageData.hasOwnProperty(key)) {
+            const value = languageData[key];
+            //console.log(`Language: ${language}, Key: ${key}, Value: ${value}`);
+
+            if(document.getElementById(key)){
+                document.getElementById(key).textContent = value;
+            }
+            // You can perform further operations with the language, key, and value here
+        }
+    }
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+}
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () =>{
+    sections.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 150;
+        let height = section.offsetHeight;
+        let id = section.getAttribute("id");
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove("active");
+                document.querySelector("header nav a[href*=" + id + "]").classList.add("active")
+            })
+        }
+    })
+
+    
+    let header = document.querySelector("header");
+
+    header.classList.toggle("sticky", window.scrollY > 100);
+
+    menuIcon.classList.remove("bx-x");
+    navbar.classList.remove("active");
+}
+
+ScrollReveal({
+    //reset: true,
+    distance: "80px",
+    duration: 2000,
+    delay: 200
+})
+
+ScrollReveal().reveal(".home-content, .heading", {origin: "top"});
+ScrollReveal().reveal(".home-img, .services-container, .portfolio-box, .contact form", {origin: "bottom"});
+ScrollReveal().reveal(".home-content h1, .about-img", {origin: "left"});
+ScrollReveal().reveal(".home-content p, .about-content", {origin: "right"});
+
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    
+    const formData = new FormData(event.target);
+    const userData = {};
+
+    for (const [key, value] of formData) {
+        userData[key] = value;
+    }
+
+    localStorage.setItem('userData', JSON.stringify(userData));
+    console.log('User data saved:', userData);
+    
+    event.target.reset();
+});
+
+const savedData = localStorage.getItem('userData');
+console.log(savedData);
